6079ca5d58874e876af9df19c1e19e8887a46ff2
Andrei Epure <epure.andrei@gmail.com>
2019-09-14 17:09:39 +0200

    Updates:
    - replace top message with monastery event
    - add the event in the table
    - update the autodate script to support Lugano-like dates

--- scripts/autodate.js
+++ scripts/autodate.js
@@ -13,18 +13,39 @@ var LUNI=[
     "decembrie"
 ];
 
+// By Single-Month-Date I mean "3 august 1990" , "3-5 august 1990"
+// the input 'parts' is already split by spaces:
+// - ["3", "august", "1990"]
+// - ["3-5", "august", "1990"]
+function parseSingleMonthDateRange(parts) {
+	var zile = parts[0].split("-").map(Number);
+	var luni = parts[1].split("-").map(l => LUNI.indexOf(l.toLowerCase()));
+	var an = Number(parts[2]);
+	var start = new Date(an, luni[0], zile[0]);
+	if (zile.length == 1) {
+		var end = new Date(an, luni[0], zile[0]);
+	} else {
+		var end = new Date(an, luni.length == 1 ? luni[0] : luni[1], zile[1]);
+	}
+	return [start, end];
+}
+
+// Can be in the form of:
+// - "3 august 1990"
+// - "3-5 august 1990"
+// - "31 august - 1 septembrie 2019"
 function parseRoDateRange(text) {
     var parts = text.split(" ");
-    var zile = parts[0].split("-").map(Number);
-    var luni = parts[1].split("-").map(l => LUNI.indexOf(l.toLowerCase()));
-    var an = Number(parts[2]);
-    var start = new Date(an, luni[0], zile[0]);
-    if (zile.length == 1) {
-        var end = new Date(an, luni[0], zile[0]);
-    } else {
-        var end = new Date(an, luni.length == 1 ? luni[0] : luni[1], zile[1]);
-    }
-    return [start, end]
+	// 
+	if (parts.length == 3) {
+		return parseSingleMonthDateRange(parts);
+	} else if (parts.length == 6) {
+		// parts is like ["31", "august", "-", "1", "septembrie", "2019"]
+		// we only care about the end date
+		return parseSingleMonthDateRange(parts.slice(3, 6));
+	}
+	// unsupported date format
+	return [new Date(), new Date()]
 };
 
 function markPastEvents(id) {
